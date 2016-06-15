888ccf31c1b9ca99f3f3b1b49566480cf9d176b4
John McKim <johncmckim@gmail.com>
2016-06-15 22:47:06 +1000

    Move logging up before running sls

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
   fi
 fi
 
-echo "Deploying stage $STAGE"
+echo "Deploying stage from branch $TRAVIS_BRANCH to $STAGE"
 
 if [ -z ${STAGE+x} ]; then
   echo "Not deploying changes";
@@ -40,16 +40,14 @@ function runSls {
   exec $cmd
 }
 
-runSls "project init"
-
-runSls "meta sync" # get new variables
-runSls "resources deploy"
-
 LOCAL_META_VAR_FILE="_meta/variables/s-variables-$STAGE-$REGION_VAR_NAME.json"
 S3_META_VAR_FILE="s3://$META_SYNC_BUCKET/serverless/$PROJECT/variables/s-variables-$STAGE-$REGION_VAR_NAME.json"
-echo "Copying metadata from $LOCAL_META_VAR_FILE to $S3_META_VAR_FILE"
-run "aws s3 cp $LOCAL_META_VAR_FILE $S3_META_VAR_FILE"  # store new variables
+echo "Will copy metadata from $LOCAL_META_VAR_FILE to $S3_META_VAR_FILE"
 
+runSls "project init"
+runSls "meta sync" # get new variables
+runSls "resources deploy"
+run "aws s3 cp $LOCAL_META_VAR_FILE $S3_META_VAR_FILE"  # store new variables
 runSls "function deploy -a"
 runSls "event deploy -a"
 runSls "endpoint deploy -a"
--- setup-deploy-envvars.sh
+++ setup-deploy-envvars.sh
@@ -1,8 +1,13 @@
+export PROJECT=cd-example
+export META_SYNC_BUCKET=cd-example-meta-sync
+
 export REGION=us-east-1
+export REGION_VAR_NAME=useast1
+
 export TRAVIS_TEST_RESULT=0
-export TRAVIS_PULL_REQUEST=false
+export TRAVIS_PULL_REQUEST="false"
 export TRAVIS_BRANCH=$(git symbolic-ref --short -q HEAD)
 
-echo "Region: $REGION"
+echo "Region: $REGION or $REGION_VAR_NAME"
 echo "Branch: $TRAVIS_BRANCH"
 echo "Is Pull Request: $TRAVIS_PULL_REQUEST"
