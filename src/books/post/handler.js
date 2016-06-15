6f951ab8a8934481b482a986eb65446e1c658454
John McKim <johncmckim@gmail.com>
2016-06-15 20:56:23 +1000

    Add sns event and lambda to handle event

--- src/books/post/handler.js
+++ src/books/post/handler.js
@@ -1,10 +1,30 @@
 'use strict';
 
+
 module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    stage: process.env.SERVERLESS_STAGE,
-    app_version: 1,
-    method: event.http_method,
-    message: 'success'
+  var message = {
+    type: 'added',
+    name: event.body.name
+  };
+
+  var params = {
+    Message: JSON.stringify(message),
+    TopicArn: process.env.WORKER_SNS_TOPIC_ARN
+  };
+
+  sns.publish(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      return context.fail('Unexpected Error')
+    } else {
+
+      return cb(null, {
+        stage: process.env.SERVERLESS_STAGE,
+        app_version: 1,
+        method: event.http_method,
+        message: 'success'
+      });
+    }
   });
+
 };
